979fd5af1fbb3d543f09ef2333d44c7714c722ba
Ammar <ammar_98@live.com>
2023-05-30 23:40:38 +0400

    Added basic components

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css'
+import './App.css';
+import { About, Footer, Header, Skills, Testimonial, Work } from './containers';
 
 function App() {
 
 	return (
-		<div>
-			App
+		<div className='app'>
+			<Header />
+			<About />
+			<Work />
+			<Skills />
+			<Testimonial />
+			<Footer />
 		</div>
   )
 }
