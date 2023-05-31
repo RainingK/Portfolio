8c1ff7ae86bfa542e8f605d7452c8fceb5e0e178
Ammar <ammar_98@live.com>
2023-05-31 17:13:18 +0400

    Made Header File

--- frontend_react/src/containers/Header/Header.tsx
+++ frontend_react/src/containers/Header/Header.tsx
@@ -2,6 +2,17 @@ import { motion } from 'framer-motion';
 import './Header.scss';
 import images from '../../constants/images';
 
+const scaleVariants = {
+	whileInView: {
+		scale: [0, 1],
+		opacity: [0, 1],
+		transition: {
+			duration: 1,
+			ease: 'easeInOut'
+		}
+	}
+}
+
 const Header = () => {
 	return (
 		<div className='app__header app__flex'>
@@ -41,7 +52,17 @@ const Header = () => {
 
 			</motion.div>
 
-			<motion.div></motion.div>
+			<motion.div
+				variants={scaleVariants}
+				whileInView={scaleVariants.whileInView}
+				className='app__header-circles'
+			>
+				{[images.flutter, images.redux, images.sass].map((circle, index) => (
+					<div className='circle-cmp app__flex' key={`circle-${index}`}>
+						<img src={circle} alt="circle" />
+					</div>
+				))}
+			</motion.div>
 
 		</div>
 	);
