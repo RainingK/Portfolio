80d0b30f10054e276b97af35ac8ba6a3485bc191
Ammar <37976857+RainingK@users.noreply.github.com>
2023-06-01 12:27:09 +0400

    About (#10)
    * Update About.tsx
    
    * Added About Page
    
    * Added About scss
    
    * Gets data from sanity
    
    * Added ENV
    
    * Changed deployment to main
    
    * Added ENV

---
+++ frontend_react/src/client.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+export const client = createClient({
+	projectId: import.meta.env.VITE_PROJECT_ID,
+	dataset: "production",
+	apiVersion: "2022-02-01",
+	useCdn: true,
+	token: import.meta.env.VITE_SANITY_TOKEN,
+})
+
+const builder = imageUrlBuilder(client);
+export const urlFor = (source: SanityImageSource) => builder.image(source);
